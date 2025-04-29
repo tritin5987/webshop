8a56f9f947b559387f64cf01583747a3ad646300
Tri <bdtcloner1@gmail.com>
2025-04-29 13:35:28 +0700

    huy phan quyen

--- application/views/Admin/layouts/header.php
+++ application/views/Admin/layouts/header.php
@@ -47,7 +47,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="<?php echo base_url(); ?>" class="brand-link">
+      <a href="<?php echo base_url('admin/'); ?>" class="brand-link">
         <img src="<?php echo base_url('public/admin/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">Quản Lý Cửa Hàng</span>
